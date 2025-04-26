f43e1d4c64ea79b91fed62d77f46c2a2c0ad31df
Vincent Botoku <desarrollador@grupolvt.com>
2025-04-26 10:10:16 -0600

    fix: removes ts and eslint error for any

--- src/lib/excelConvert.ts
+++ src/lib/excelConvert.ts
@@ -3,7 +3,7 @@ import * as XLSX from "xlsx"
 
 export const onGetExport = async (data:Users[],title?: string, worksheetname?: string) => {
     try {
-        const dataToExport = data.map((info:any) => ({
+        const dataToExport = data.map((info) => ({
             firstName: info.firstName,
             lastName: info.lastName,
             teamName: info.fplTeam
@@ -16,7 +16,11 @@ export const onGetExport = async (data:Users[],title?: string, worksheetname?: s
           // Save the workbook as an Excel file
           XLSX.writeFile(workbook, `${title}.xlsx`);
           console.log(`Exported data to ${title}.xlsx`);
-    } catch (error:any) {
-        console.log("#==================Export Error", error.message);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log("#==================Export Error", error.message);
+        } else {
+            console.log("#==================Export Error", String(error));
+        }
     }
 }
