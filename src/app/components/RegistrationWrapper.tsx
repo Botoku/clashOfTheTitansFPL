2d1244d0ad6dfeaba1cabc89a82ced5b69be40f3
Vincent Botoku <desarrollador@grupolvt.com>
2025-10-12 16:27:05 -0600

    fix: stops form upload

--- src/app/components/RegistrationWrapper.tsx
+++ src/app/components/RegistrationWrapper.tsx
@@ -5,7 +5,8 @@ import RegistrationForm from "./RegistrationForm";
 const RegistrationWrapper = () => {
   return (
     <div className="max-w-screen">
-      <RegistrationForm  />
+      <h2 className="text-center">Registration closed</h2>
+      <p className="text-center">Reopens 2026</p>
     </div>
   );
 };
